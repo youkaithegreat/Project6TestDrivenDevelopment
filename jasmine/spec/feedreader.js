c4fea7142ce6695d3f38baaf49607741b82aabcd
Kevin <yangkevint@gmail.com>
2016-02-12 11:36:10 +0900

    new: Added in 90 percent of the tests.
    Added in tests to check entries, check menu functionality, check arraylist

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,32 +44,35 @@ $(function() {
 
     describe('The menu', function(){
         it('is hidden by default', function(){
-            var menu = $(".menu-hidden");
-            expect(menu).toEqual().toBe(0);
+            expect($("body").hasClass("menu-hidden")).toBe(true);
         });
 
+        it('toggles when clicked', function(){
+            $('.menu-icon-link').click();
+
+                expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        })
     });
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        describe('Initial Entries', function() {
+            beforeEach(function(done) {
+                loadFeed(0, function() {
+                    done();
+                });
+            });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+            it('has more than 0 entries', function() {
+                expect($('.entry-link').length).toBeGreaterThan(0);
+            });
+        });
+
+
+    
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
